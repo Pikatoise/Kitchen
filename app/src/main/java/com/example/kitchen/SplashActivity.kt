e51421558bf77f53c631ba1f53a2692cf86afd34
Pikatoise <wierwolf@inbox.ru>
2024-05-19 18:18:29 +0500

    Change splash animation

--- app/src/main/java/com/example/kitchen/SplashActivity.kt
+++ app/src/main/java/com/example/kitchen/SplashActivity.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.kitchen.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -21,14 +19,12 @@ class SplashActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.ivLogo.alpha = 0f
-        binding.ivLogo.scaleX = 0.8f
-        binding.ivLogo.scaleY = 0.8f
-        binding.ivLogo.animate().setDuration(1500).alpha(1f ).scaleX(1f).scaleY(1f).withEndAction {
+        binding.ivLogo.animate().setDuration(1500).alpha(1f ).withEndAction {
             val i = Intent(this, AuthActivity::class.java)
 
             startActivity(i)
 
-            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out,)
 
             finish()
         }
