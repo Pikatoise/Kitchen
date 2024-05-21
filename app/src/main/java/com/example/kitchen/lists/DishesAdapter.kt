a7026a76e27b95211fa5a91093c2968023ba97c7
Pikatoise <wierwolf@inbox.ru>
2024-05-21 19:55:20 +0500

    Alot of new

---
+++ app/src/main/java/com/example/kitchen/lists/DishesAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.kitchen.lists
+
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.graphics.drawable.BitmapDrawable
+import android.os.Build
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.core.graphics.drawable.toBitmap
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kitchen.DownloadImageTask
+import com.example.kitchen.R
+import com.example.kitchen.models.Dish
+import com.google.android.material.card.MaterialCardView
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
+
+class DishesAdapter constructor(
+    private val dishes: List<Dish>,
+    private val likesCount: List<Int>) : RecyclerView.Adapter<DishesAdapter.MyViewHolder>(){
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val imageView: ImageView = itemView.findViewById(R.id.iv_dish_image)
+        val tvTitle: TextView = itemView.findViewById(R.id.tv_dish_title)
+        val tvLikeCount: TextView = itemView.findViewById(R.id.tv_dish_like_count)
+        val tvCookingTime: TextView = itemView.findViewById(R.id.tv_dish_cooking_time)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.dish_item, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return dishes.count()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        DownloadImageTask(holder.imageView)
+            .execute("https://gkeqyqnfnwgcbpgbnxkq.supabase.co/storage/v1/object/public/kitchen_dish_image/${dishes[position].image}")
+
+        holder.tvTitle.text = dishes[position].name
+        holder.tvLikeCount.text = likesCount[position].toString()
+        holder.tvCookingTime.text = "${dishes[position].cookingTime} мин."
+    }
+}
--- app/src/main/java/com/example/kitchen/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/kitchen/ui/home/HomeFragment.kt
@@ -1,61 +1,81 @@
 package com.example.kitchen.ui.home
 
-import android.app.ProgressDialog
-import android.graphics.RenderEffect
-import android.graphics.Shader
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import com.example.kitchen.R
+import com.example.kitchen.DownloadImageTask
 import com.example.kitchen.databinding.FragmentHomeBinding
+import com.example.kitchen.lists.DishesAdapter
+import com.example.kitchen.lists.LinePagerIndicatorDecoration
+import com.example.kitchen.models.Dish
 import com.example.kitchen.sqlite.PreferencesRepository
 import com.example.kitchen.supabase.SupabaseModule
 import com.example.kitchen.supabase.interfaces.ProfileRepository
 import com.example.kitchen.supabase.repositories.ProfileRepositoryImpl
-import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
     private lateinit var profileRepository: ProfileRepository
+    private var profileId: Int = -1
 
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        profileRepository = ProfileRepositoryImpl(SupabaseModule.provideSupabaseDatabase())
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        val profileId = PreferencesRepository(this.requireContext()).getProfileId()
-        binding.textHome.text = "This is Home Page!!!\n Profile: ${profileId}"
-        binding.textHome.setRenderEffect(RenderEffect.createBlurEffect(10f,10f, Shader.TileMode.MIRROR))
+        profileRepository = ProfileRepositoryImpl(SupabaseModule.provideSupabaseDatabase())
+        profileId = PreferencesRepository(this.requireContext()).getProfileId()
+        binding.rvHomeRandomDishes.addItemDecoration(LinePagerIndicatorDecoration())
+        //binding.textHome
 
-        binding.buttonStart.setOnClickListener {
-            var name = "UNDEFINED"
-
-            val progressDialog = ProgressDialog.show(activity, "", "")
-            progressDialog.show()
-            progressDialog.setContentView(R.layout.progress_dialog)
-            progressDialog.window!!.setBackgroundDrawableResource(android.R.color.transparent)
-
-            lifecycleScope.launch {
-                val profile = profileRepository.getProfile(profileId)
-
-                if (profile != null)
-                    name = profile.name
-            }.invokeOnCompletion {
-                progressDialog.dismiss()
-                
-                Toast.makeText(this.requireContext(),name, Toast.LENGTH_SHORT).show()
-            }
-        }
+        loadRandomDishes()
 
         return binding.root
+    }
+
+    private fun loadRandomDishes(){
+        var listDishes: ArrayList<Dish> = arrayListOf()
+        var listLikes: ArrayList<Int> = arrayListOf()
+
+        listDishes.add(Dish(
+            1,
+            "Блины классические",
+            "Очень сложный рецепт",
+            "dish_1.jpg",
+            30,
+            2,
+            1,
+            7))
+        listDishes.add(
+            Dish(
+                2,
+                "Блины не классические",
+                "Очень сложный рецепт",
+                "dish_1.jpg",
+                35,
+                3,
+                1,
+                7))
+        listDishes.add(
+            Dish(
+                3,
+                "Блины ультра",
+                "Очень сложный рецепт",
+                "dish_1.jpg",
+                60,
+                10,
+                1,
+                7))
+
+        listLikes.add(15)
+        listLikes.add(3)
+        listLikes.add(100)
+
+        binding.rvHomeRandomDishes.adapter = DishesAdapter(listDishes, listLikes)
     }
 
     override fun onDestroyView() {
