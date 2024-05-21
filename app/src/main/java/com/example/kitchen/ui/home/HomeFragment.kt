f64cfd8faad577f1899c320468e2ce1eb1cc1e57
Pikatoise <wierwolf@inbox.ru>
2024-05-21 16:54:05 +0500

    Create dish item

--- app/src/main/java/com/example/kitchen/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/kitchen/ui/home/HomeFragment.kt
@@ -1,11 +1,15 @@
 package com.example.kitchen.ui.home
 
 import android.app.ProgressDialog
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.example.kitchen.R
@@ -21,6 +25,7 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var profileRepository: ProfileRepository
 
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         profileRepository = ProfileRepositoryImpl(SupabaseModule.provideSupabaseDatabase())
 
@@ -28,6 +33,7 @@ class HomeFragment : Fragment() {
 
         val profileId = PreferencesRepository(this.requireContext()).getProfileId()
         binding.textHome.text = "This is Home Page!!!\n Profile: ${profileId}"
+        binding.textHome.setRenderEffect(RenderEffect.createBlurEffect(10f,10f, Shader.TileMode.MIRROR))
 
         binding.buttonStart.setOnClickListener {
             var name = "UNDEFINED"
